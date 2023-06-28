3ec103da79d99d43ee087b3b08340678792605f5
Guido Ormeño <ormenoguidofrancisco@gmail.com>
2023-06-28 02:31:44 -0300

    set y get estudiantes

--- Proyecto1/AppCoder/urls.py
+++ Proyecto1/AppCoder/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
-from AppCoder.views import  inicio,cursos,entregables,estudiantes,profesores
+from AppCoder.views import  *
 
 urlpatterns = [
     path('inicio/', inicio),
     path('cursos/', cursos, name="Cursos"),
     path('entregables/', entregables, name="Entregables"),
     path('estudiantes/', estudiantes, name="Estudiantes"),
-    path('profesores/', profesores, name="Profesores")
-]
+    path('profesores/', profesores, name="Profesores"),
+    path('setEstudiantes/', setEstudiantes, name="SetEstudiantes"),
+    path('getEstudiante/', getEstudiante, name="GetEstudiante"),
+    path('buscarEstudiante/', buscarEstudiante, name="BuscarEstudiante")
+]   
--- Proyecto1/AppCoder/views.py
+++ Proyecto1/AppCoder/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
+from AppCoder.forms import formSetEstudiante
+from AppCoder.models import Estudiante
 
 
 def inicio(request):
@@ -17,4 +19,30 @@ def estudiantes(request):
 
 def entregables(request):
     return render(request, "AppCoder/entregables.html")
+
+def setEstudiantes(request):
+    if request.method == 'POST':
+        miFormulario = formSetEstudiante(request.POST)
+        print(miFormulario)
+        if miFormulario.is_valid:
+            data = miFormulario.cleaned_data
+            estudiante = Estudiante(nombre=data["nombre"],apellido=data["apellido"], email = data["email"])
+            estudiante.save()
+            return render(request, "AppCoder/inicio.html")
+    else:
+        miFormulario = formSetEstudiante()
+        return render(request, "AppCoder/setEstudiantes.html", {"miFormulario":miFormulario})
+
+def getEstudiante(request):
+    return render (request, "AppCoder/getEstudiante.html")
+
+def buscarEstudiante(request):
+    if request.GET["nombre"]:
+        nombre= request.GET["nombre"]
+        estudiantes = Estudiante.objects.filter(nombre= nombre)
+        return render(request, "AppCoder/getEstudiante.html", {"estudiantes":estudiantes})
+    else:
+        respuesta = "No se enviaron datos"
+    return HttpResponse(respuesta)
 # Create your views here.
+
